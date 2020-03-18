253ea97abb724133c12ae446c776e12d8863d8be
ali <aliyoussef34@hotmail.fr>
2020-03-18 11:11:47 +0100

    wrapped the layouts in a refresh layout to refresh the content when the user swipes down

--- app/src/main/java/com/example/devjobtracker/MainActivity.kt
+++ app/src/main/java/com/example/devjobtracker/MainActivity.kt
@@ -15,12 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Whenever we swipe down (to refresh the page) it will recall fetchJobOffers() to get the latest offers available
+        refreshLayout.setOnRefreshListener {
+            fetchJobOffers()
+        }
+        //to fetch the offers whenever the ui is displayed
+        fetchJobOffers()
+    }
+
+
+
+    //Fetches all Offers (Positions) from the API
+    private fun fetchJobOffers() {
+        refreshLayout.isRefreshing = true;
+
         JobOfferAPI().getPositions().enqueue(object: Callback<List<JobOffer>> {
             override fun onFailure(call: Call<List<JobOffer>>, t: Throwable) {
+                refreshLayout.isRefreshing = false;
                 Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
             }
 
             override fun onResponse(call: Call<List<JobOffer>>, response: Response<List<JobOffer>>) {
+                refreshLayout.isRefreshing = false;
                 val jobOffers = response.body()
 
                 jobOffers?.let{
@@ -30,7 +46,6 @@ class MainActivity : AppCompatActivity() {
             }
         } )
     }
-
 
 
     private fun showJobOffers(jobOffers: List<JobOffer>) {

eb29748dbe3d69a2338851bd49156f49187fb278
aliYous <aliyoussef34@hotmail.fr>
2020-03-23 18:30:41 +0100

    added 'popular technologies' title on top of linear layout

--- app/src/main/java/com/example/devjobtracker/MainActivity.kt
+++ app/src/main/java/com/example/devjobtracker/MainActivity.kt
@@ -10,7 +10,6 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/devjobtracker/ProgrammingLanguage.kt
@@ -0,0 +1,7 @@
+package com.example.devjobtracker
+
+class ProgrammingLanguage {
+    private var image: Int = 0
+    private var name: String = "Language Name"
+
+}

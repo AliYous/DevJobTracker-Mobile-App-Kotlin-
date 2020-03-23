b7f69f190f2173d4acda843fd345795ae027bead
aliYous <aliyoussef34@hotmail.fr>
2020-03-23 20:14:17 +0100

    horizontal recyvler functionnal

--- app/src/main/java/com/example/devjobtracker/MainActivity.kt
+++ app/src/main/java/com/example/devjobtracker/MainActivity.kt
@@ -11,13 +11,6 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
-    //Creating languages manually to test
-    val language1 = ProgrammingLanguage("Python")
-    val language2 = ProgrammingLanguage("Ruby")
-    val language3 = ProgrammingLanguage("JavaScript")
-    val language4 = ProgrammingLanguage("React")
-    val language5 = ProgrammingLanguage("Java")
-    val progLanguages = listOf(language1, language2, language3, language4, language5)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +20,14 @@ class MainActivity : AppCompatActivity() {
         refreshLayout.setOnRefreshListener {
             fetchJobOffers()
         }
+
+        //Creating languages manually to test
+        val language1 = ProgrammingLanguage("Python")
+        val language2 = ProgrammingLanguage("Ruby")
+        val language3 = ProgrammingLanguage("JavaScript")
+        val language4 = ProgrammingLanguage("React")
+        val language5 = ProgrammingLanguage("Java")
+        val progLanguages = listOf(language1, language2, language3, language4, language5)
         //to fetch the offers whenever the ui is displayed
         showProgLanguages(progLanguages)
         fetchJobOffers()
@@ -75,7 +76,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showProgLanguages(progLanguages: List<ProgrammingLanguage>) {
-        recyclerViewLanguages.layoutManager = LinearLayoutManager(this)
+        recyclerViewLanguages.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerViewLanguages.adapter = ProgrammingLanguageAdapter(progLanguages)
     }
 }
--- app/src/main/java/com/example/devjobtracker/ProgrammingLanguageAdapter.kt
+++ app/src/main/java/com/example/devjobtracker/ProgrammingLanguageAdapter.kt
@@ -4,21 +4,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.layout_offer.view.*
+import kotlinx.android.synthetic.main.layout_language.view.*
 
 class ProgrammingLanguageAdapter(private val programmingLanguages : List<ProgrammingLanguage>) : RecyclerView.Adapter<ProgrammingLanguageAdapter.ProgrammingLanguageViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgrammingLanguageAdapter.ProgrammingLanguageViewHolder {
-        return ProgrammingLanguageAdapter.ProgrammingLanguageViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgrammingLanguageViewHolder {
+        return ProgrammingLanguageViewHolder(
             LayoutInflater.from(parent.context)
                 .inflate(R.layout.layout_language, parent, false)
         )
     }
     override fun getItemCount() = programmingLanguages.size
 
-    override fun onBindViewHolder(holder: ProgrammingLanguageAdapter.ProgrammingLanguageViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ProgrammingLanguageViewHolder, position: Int) {
         val programmingLanguage = programmingLanguages[position]
-        holder.view.textViewTitle.text = programmingLanguage.name
+        holder.view.textViewTitleLang.text = "Ruby"//programmingLanguage.name
     }
 
 
